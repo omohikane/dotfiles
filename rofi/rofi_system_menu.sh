6317d133221ca9c27bbe5326128d54ef8445179a
omohikane <koki.okino765@gmail.com>
2018-08-27 16:05:50 +0900

    latest

--- rofi/rofi_system_menu.sh
+++ rofi/rofi_system_menu.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
-declare -A list=(
-  ##  LOCK  ##
-  ['Lock']='gnome-screensaver-command -l'
-  ['Logout']='gnome-session-quit --force'
-  ##  POWER  ##
-  ['Suspend']='systemctl suspend'
-  ['Shutdown']='systemctl poweroff'
-  ['Reboot']='systemctl reboot'
-)
-
-if [[ ${1##* } == 'yes' ]]; then
-  eval ${list[${1%% *}]}
-elif [[ ${1##* } == 'no' ]]; then
-  echo ${!list[@]} | sed 's/ /\n/g'
-elif [[ -n $1 ]]; then
-  echo "$1 / no"
-  echo "$1 / yes"
+if [[ -z $@ ]]; then
+    OPTIONS="Power-off\nReboot\nSuspend\nHibernate"
+    OPTIONS="Suspend\nLock\nReboot\nShutdown"
+    echo -e $OPTIONS
 else
-  echo ${!list[@]} | sed 's/ /\n/g'
+    case $option in
+    Reboot)
+        systemctl reboot ;;
+    Shutdown)
+        systemctl poweroff ;;
+    Suspend)
+        systemctl suspend ;;
+    Hibernate)
+        systemctl hibernate ;;
+    Lock)
+	lock ;;    
+    *)
+        ;;
+    esac
 fi
+
+
