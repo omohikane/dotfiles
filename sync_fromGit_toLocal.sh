79d15658302d326b82c2400dd8de11b84076e9e8
omohikane <koki.okino765@gmail.com>
2019-03-04 14:57:15 +0900

    modify shortcut

---
+++ sync_fromGit_toLocal.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+#i3
+cp -r /home/ripple/git/dotfiles/i3 /home/ripple/.config/
+
+#rofi
+cp -r /home/ripple/git/dotfiles/rofi /home/ripple/.config/
+
+#conky
+cp -r /home/ripple/git/dotfiles/conky /home/ripple/.config/ 
+
---
+++ sync_fromLocal_toGit.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+#i3
+cp -r /home/ripple/.config/i3 /home/ripple/git/dotfiles/
+
+#rofi
+cp -r /home/ripple/.config/rofi /home/ripple/git/dotfiles/
+
+#conky
+cp -r /home/ripple/.config/conky /home/ripple/git/dotfiles/
+
