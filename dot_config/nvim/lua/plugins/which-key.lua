32bcf3916167008b72978c9c4851a6f708e2796e
r1ppl3 <r1ppl3@vivaldi.net>
2025-05-26 10:58:41 +0900

    fix whichkey

--- dot_config/nvim/lua/plugins/which-key.lua
+++ dot_config/nvim/lua/plugins/which-key.lua
@@ -19,18 +19,5 @@ return function()
     },
   })
 
-wk.register({
-  { "<leader>f", group = "file" },
-  { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find File" },
-  { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent Files" },
-  { "<leader>b", group = "buffer" },
-  { "<leader>bd", "<cmd>bdelete<cr>", desc = "Delete Buffer" },
-  { "<leader>bn", "<cmd>enew<cr>", desc = "New Buffer" },
-  { "<leader>g", group = "git" },
-  { "<leader>gs", "<cmd>GinStatus<cr>", desc = "Git Status" },
-  { "<leader>t", group = "terminal" },
-  { "<leader>tt", "<cmd>ToggleTerm<cr>", desc = "Toggle Terminal" },
-})
-
 end
 
