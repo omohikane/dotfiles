f6b74595c6c30b0860d2a8ebac3ec033ef604bec
r1ppl3 <r1ppl3@vivaldi.net>
2025-05-26 19:26:39 +0900

    add-trouble

--- dot_config/nvim/lua/plugins/trouble.lua
+++ dot_config/nvim/lua/plugins/trouble.lua
@@ -1,23 +1,37 @@
 -- lua/plugins/trouble.lua
-require("trouble").setup({
+
+local trouble = require("trouble")
+
+trouble.setup({
   position = "bottom",
   height = 10,
   icons = true,
-  fold_open = "",
-  fold_closed = "",
-  signs = {
-    error = "",
-    warning = "",
-    hint = "",
-    information = "",
-    other = "",
+  mode = "workspace_diagnostics",
+  group = true,
+  padding = true,
+  action_keys = {
+    close = "q",
+    refresh = "r",
+    jump = { "<cr>", "<tab>" },
   },
-  use_diagnostic_signs = true,
+  auto_open = false,
+  auto_close = false,
+  auto_preview = true,
+  signs = {
+    error = "",
+    warning = "",
+    hint = "",
+    information = "",
+    other = "",
+  },
+  use_diagnostic_signs = false
 })
 
-vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>", { desc = "Toggle Trouble" })
+-- 🔐 Key mappings
+vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>", { desc = "Toggle Trouble Panel" })
 vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", { desc = "Workspace Diagnostics" })
 vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", { desc = "Document Diagnostics" })
-vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>", { desc = "Location List" })
 vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", { desc = "Quickfix List" })
+vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>", { desc = "Location List" })
+vim.keymap.set("n", "gr", "<cmd>TroubleToggle lsp_references<cr>", { desc = "LSP References (Trouble)" })
 
