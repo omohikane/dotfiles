fee152c746fa7cb612d937939eae533d26929ac3
r1ppl3 <r1ppl3@vivaldi.net>
2025-05-26 12:26:20 +0900

    update-lsp-setting

--- dot_config/nvim/lua/lsp/lua_ls.lua
+++ dot_config/nvim/lua/lsp/lua_ls.lua
@@ -1,4 +1,5 @@
 -- lua/lsp/lua_ls.lua
+
 local config = require("lsp.config")
 
 return {
@@ -8,11 +9,18 @@ return {
   capabilities = config.capabilities,
   settings = {
     Lua = {
+      runtime = {
+        version = "LuaJIT",
+      },
       diagnostics = {
         globals = { "vim" },
       },
       workspace = {
         checkThirdParty = false,
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      telemetry = {
+        enable = false,
       },
     },
   },
