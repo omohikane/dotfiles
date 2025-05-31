20961669ab08f54e6a7861d3825b8817ca4904fe
r1ppl3 <r1ppl3@vivaldi.net>
2025-06-01 00:22:01 +0900

    update codeium

--- dot_config/nvim/lua/ai/codeium.lua
+++ dot_config/nvim/lua/ai/codeium.lua
@@ -1,13 +1,19 @@
--- lua/ai/codeium.lua
+-- Disable default bindings
+vim.g.codeium_disable_bindings = true
 
-require("codeium").setup({
-	enable_chat = false,
-	tools = {
-		keymaps = {
-			accept = "<C-y>",
-			next = "<C-n>",
-			prev = "<C-p>",
-			clear = "<C-x>",
-		},
-	},
-})
+-- AI補完のキーマッピング
+vim.keymap.set("i", "<C-g>", function()
+	return vim.fn["codeium#Accept"]()
+end, { expr = true, silent = true, desc = "Codeium: Accept suggestion" })
+
+vim.keymap.set("i", "<C-;>", function()
+	return vim.fn
+end, { expr = true, silent = true, desc = "Codeium: Next suggestion" })
+
+vim.keymap.set("i", "<C-,>", function()
+	return vim.fn["codeium#CycleCompletions"](-1)
+end, { expr = true, silent = true, desc = "Codeium: Previous suggestion" })
+
+vim.keymap.set("i", "<C-x>", function()
+	return vim.fn["codeium#Clear"]()
+end, { expr = true, silent = true, desc = "Codeium: Clear suggestion" })
