f3cdb2a0967636b6747c3d2bafc0e8401a9e8a07
r1ppl3 <r1ppl3@vivaldi.net>
2025-06-01 11:12:15 +0900

    fix noice

--- dot_config/nvim/lua/plugins/noice.lua
+++ dot_config/nvim/lua/plugins/noice.lua
@@ -2,22 +2,83 @@
 
 local ok, noice = pcall(require, "noice")
 if not ok then
-  vim.notify("Noice not loaded", vim.log.levels.WARN)
-  return
+	vim.notify("Noice not loaded", vim.log.levels.WARN)
+	return
 end
 
 noice.setup({
-  lsp = {
-    override = {
-      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-      ["vim.lsp.util.stylize_markdown"] = true,
-      ["cmp.entry.get_documentation"] = true,
-    },
-    progress = { enabled = false },
-  },
-  messages = { enabled = false },
-  cmdline = { enabled = false },
-  views = {},
-  presets = {},
-})
+	lsp = {
+		override = {
+			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+			["vim.lsp.util.stylize_markdown"] = true,
+			["cmp.entry.get_documentation"] = true,
+		},
+		progress = { enabled = true },
+	},
 
+	messages = {
+		enabled = true,
+		view = "notify",
+		view_error = "notify",
+		view_warn = "notify",
+		view_history = "messages",
+		view_search = "virtualtext",
+	},
+
+	cmdline = {
+		enabled = true,
+		view = "cmdline_popup",
+		format = {
+			cmdline = {},
+			search_down = {},
+			search_up = {},
+			filter = {},
+			lua = {},
+			help = {},
+		},
+	},
+
+	views = {
+		cmdline_popup = {
+			border = {
+				style = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+			},
+			position = {
+				row = 5,
+				col = "50%",
+			},
+			size = {
+				width = 60,
+				height = "auto",
+			},
+			win_options = {
+				winhighlight = { Normal = "Normal", FloatBorder = "FloatBorder" },
+			},
+		},
+		popupmenu = {
+			relative = "editor",
+			position = {
+				row = 8,
+				col = "50%",
+			},
+			size = {
+				width = 60,
+				height = 10,
+			},
+			border = {
+				style = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+			},
+			win_options = {
+				winhighlight = { Normal = "Normal", FloatBorder = "FloatBorder" },
+			},
+		},
+	},
+
+	presets = {
+		bottom_search = true,
+		command_palette = true,
+		long_message_to_split = true,
+		inc_rename = false,
+		lsp_doc_border = true,
+	},
+})
