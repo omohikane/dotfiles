065887e27be41f8dce60756302cdde0c4e4ced7d
r1ppl3 <r1ppl3@vivaldi.net>
2025-06-01 21:49:51 +0900

    fix-plugin

--- dot_config/nvim/lua/plugins/vim-sandwich.lua
+++ dot_config/nvim/lua/plugins/vim-sandwich.lua
@@ -1,10 +1,9 @@
 -- plugins/vim-sandwich.lua
 
 vim.schedule(function()
-  if vim.fn.exists("*sandwich#default_recipes#load") == 1 then
-    vim.g["sandwich#recipes"] = vim.fn["sandwich#default_recipes#load"]()
-  else
-    vim.notify("⚠ sandwich#default_recipes#load が利用できませんでした", vim.log.levels.WARN)
-  end
+	if vim.fn.exists("*sandwich#default_recipes#load") == 1 and vim.fn.exists("*sandwich#recipe#extend") == 1 then
+		vim.g["sandwich#recipes"] = vim.fn["sandwich#default_recipes#load"]()
+	else
+		vim.notify("⚠ sandwich#default_recipes#load が利用できませんでした", vim.log.levels.WARN)
+	end
 end)
-
