f7b070bb2b7720e1a0b6c8995fbf2f1bf7408f8b
r1ppl3 <r1ppl3@vivaldi.net>
2025-06-01 09:32:00 +0900

    fix windsurf

--- dot_config/nvim/lua/ai/windsurf.lua
+++ dot_config/nvim/lua/ai/windsurf.lua
@@ -1,19 +1,19 @@
 -- Disable default bindings
-vim.g.codeium_disable_bindings = true
+vim.g.windsurf_disable_bindings = true
 
 -- AI補完のキーマッピング
 vim.keymap.set("i", "<C-g>", function()
-	return vim.fn["codeium#Accept"]()
-end, { expr = true, silent = true, desc = "Codeium: Accept suggestion" })
+	return vim.fn["windsurf#Accept"]()
+end, { expr = true, silent = true, desc = "windsurf: Accept suggestion" })
 
 vim.keymap.set("i", "<C-;>", function()
 	return vim.fn
-end, { expr = true, silent = true, desc = "Codeium: Next suggestion" })
+end, { expr = true, silent = true, desc = "windsurf: Next suggestion" })
 
 vim.keymap.set("i", "<C-,>", function()
-	return vim.fn["codeium#CycleCompletions"](-1)
-end, { expr = true, silent = true, desc = "Codeium: Previous suggestion" })
+	return vim.fn["windsurf#CycleCompletions"](-1)
+end, { expr = true, silent = true, desc = "windsurf: Previous suggestion" })
 
 vim.keymap.set("i", "<C-x>", function()
-	return vim.fn["codeium#Clear"]()
-end, { expr = true, silent = true, desc = "Codeium: Clear suggestion" })
+	return vim.fn["windsurf#Clear"]()
+end, { expr = true, silent = true, desc = "windsurf: Clear suggestion" })
