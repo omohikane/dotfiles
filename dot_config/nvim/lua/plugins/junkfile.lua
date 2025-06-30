a449c5d03def422754cf3e88e027dbbe93e3ef8a
r1ppl3 <r1ppl3@vivaldi.net>
2025-06-30 14:00:07 +0900

    add-junkfile

---
+++ dot_config/nvim/lua/plugins/junkfile.lua
@@ -0,0 +1,14 @@
+-- plugins/junkfile.lua
+
+-- config
+vim.g["junkfile#directory"] = vim.fn.expand("~/.junk/")
+vim.g["junkfilename_format"] = "%Y%m%d-%H%M%S.txt"
+
+-- Create directory
+local junk_dir = vim.fn.expand("~/.junk/")
+if vim.fn.isdirectory(junk_dir) == 0 then
+	vim.fn.mkdir(junk_dir, "p")
+end
+
+-- command shortcut
+vim.keymap.set("n", "<leader>jf", ":Junkfile<CR>", { noremap = true, silent = true, dsec = "Open junk file" })
