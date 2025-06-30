184ff615937fe3fb5823b11a64ceeee2d6caf4e9
r1ppl3 <r1ppl3@vivaldi.net>
2025-06-30 14:48:41 +0900

    update junkfile config

--- dot_config/nvim/lua/plugins/junkfile.lua
+++ dot_config/nvim/lua/plugins/junkfile.lua
@@ -1,14 +1,21 @@
 -- plugins/junkfile.lua
 
--- config
-vim.g.junkfile_directory = vim.fn.expand("~/.junk/")
-vim.g.junkfile_name_format = "%Y%m%d-%H%M%S.txt"
+vim.api.nvim_create_autocmd("User", {
+	pattern = "DenopsReady",
+	callback = function()
+		vim.g.junkfile_directory = vim.fn.expand("~/.junk/")
+		vim.g.junkfile_name_format = "%Y%m%d-%H%M%S.txt"
 
--- Create directory
-local junk_dir = vim.fn.expand("~/.junk/")
-if vim.fn.isdirectory(junk_dir) == 0 then
-	vim.fn.mkdir(junk_dir, "p")
-end
+		local junk_dir = vim.g.junkfile_directory
+		if vim.fn.isdirectory(junk_dir) == 0 then
+			vim.fn.mkdir(junk_dir, "p")
+		end
 
--- command shortcut
-vim.keymap.set("n", "<leader>jf", ":Junkfile<CR>", { noremap = true, silent = true, desc = "Open junk file" })
+		-- shortcut
+		vim.keymap.set("n", "<leader>jf", ":Junkfile<CR>", {
+			noremap = true,
+			silent = true,
+			desc = "Open junk file",
+		})
+	end,
+})
