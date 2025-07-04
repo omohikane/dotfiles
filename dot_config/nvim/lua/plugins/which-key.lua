3f87537c617f5c61c8a790db1fdcb055b1393efe
r1ppl3 <r1ppl3@vivaldi.net>
2025-07-04 12:34:14 +0900

    update which-key

--- dot_config/nvim/lua/plugins/which-key.lua
+++ dot_config/nvim/lua/plugins/which-key.lua
@@ -1,23 +1,19 @@
 -- lua/plugins/which-key.lua
 
-return function()
-  local ok, wk = pcall(require, "which-key")
-  if not ok then
-    vim.notify("which-key.nvim が読み込めませんでした", vim.log.levels.ERROR)
-    return
-  end
-
-  wk.setup({
-    plugins = {
-      spelling = { enabled = true },
-    },
-    win = {
-      border = "single",
-    },
-    layout = {
-      align = "center",
-    },
-  })
-
+local ok, wk = pcall(require, "which-key")
+if not ok then
+	vim.notify("which-key.nvim が読み込めませんでした", vim.log.levels.ERROR)
+	return
 end
 
+wk.setup({
+	plugins = {
+		spelling = { enabled = true },
+	},
+	win = {
+		border = "single",
+	},
+	layout = {
+		align = "center",
+	},
+})
