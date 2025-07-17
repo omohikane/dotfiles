5bd4762c10760f8b65d00c51b2f4674539bd1346
r1ppl3 <r1ppl3@vivaldi.net>
2025-07-17 12:45:55 +0900

    update junkfile config

--- dot_config/nvim/lua/plugins/junkfile.lua
+++ dot_config/nvim/lua/plugins/junkfile.lua
@@ -1,39 +1,33 @@
 -- plugins/junkfile.lua
 
--- junkfile.vim がロードされた直後でも設定が反映されるように、AutoCmdの外に設定を移動
--- ただし、DenopsReadyに依存する設定（例: Denops系のプラグインとの連携）はAutoCmd内でも良い
--- junkfile.vim の設定は基本的に即時反映されるべきです
-
--- グローバル変数のキー名に '#' を含めます
+-- junkfile.vim の設定 (グローバル変数)
+-- 必ず '#' を含む形式で指定してください
 vim.g["junkfile#directory"] = vim.fn.expand("~/.junk/")
 vim.g["junkfile#filename_format"] = "%Y%m%d-%H%M%S.txt"
-vim.g["junkfile#default_filetype"] = "text" -- オプションでデフォルトファイルタイプも設定
+vim.g["junkfile#default_filetype"] = "text" -- 必要に応じて追加
 
-local junk_dir = vim.g["junkfile#directory"] -- 正しいキー名で取得
+-- ジャンクファイルディレクトリが存在しない場合は作成
+local junk_dir = vim.g["junkfile#directory"]
 if vim.fn.isdirectory(junk_dir) == 0 then
 	vim.fn.mkdir(junk_dir, "p")
 end
 
--- キーマップは、Neovim起動時に設定されるべきなのでAutoCmdの外でも良いが、
--- 確実性を期すため、プラグインロード後すぐに設定されるようにします。
--- もし DenopsReady イベントが、JunkFile コマンドが定義されるより「後」に発火するなら、
--- この AutoCmd の中に書くことは正しい選択です。
-vim.api.nvim_create_autocmd("User", {
-	pattern = "DenopsReady",
-	callback = function()
-		-- shortcut
-		vim.keymap.set("n", "<leader>jf", ":Junkfile<CR>", {
-			noremap = true,
-			silent = true,
-			desc = "Open junk file",
-		})
-	end,
+-- キーマップ設定
+-- プラグインロード時にすぐに適用されるように、AutoCmd の外に記述します。
+-- JunkFile コマンドがこの時点で利用可能であると想定します。
+vim.keymap.set("n", "<leader>jf", ":JunkFile<CR>", {
+	noremap = true,
+	silent = true,
+	desc = "Open junk file",
 })
 
--- あるいは、DenopsReadyに依存しないシンプルなキーマップであれば
--- AutoCmdの外に記述しても構いません。
--- vim.keymap.set("n", "<leader>jf", ":Junkfile<CR>", {
--- 	noremap = true,
--- 	silent = true,
--- 	desc = "Open junk file",
+-- ※注意: もし上記でダメな場合のみ、以下のように非常に遅延させてみる
+-- vim.api.nvim_create_autocmd("VimEnter", {
+-- 	callback = function()
+-- 		vim.keymap.set("n", "<leader>jf", ":JunkFile<CR>", {
+-- 			noremap = true,
+-- 	 silent = true,
+-- 			desc = "Open junk file",
+-- 		})
+-- 	end,
 -- })
