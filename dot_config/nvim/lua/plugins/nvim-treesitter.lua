94594357837de044ddf2944d03a95992bd6ff340
ripple <koki.okino765@gmail.com>
2025-10-26 22:31:36 +0900

    update treesitter configs

--- dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -2,33 +2,49 @@
 local M = {}
 
 function M.setup()
-  local ok, ts = pcall(require, "nvim-treesitter.configs")
-  if not ok then return end
+	local ok, ts = pcall(require, "nvim-treesitter.configs")
+	if not ok then
+		return
+	end
 
-  ts.setup({
-    highlight = { enable = true },
-    indent    = { enable = true },
+	ts.setup({
+		highlight = { enable = true },
+		indent = { enable = true },
 
-    ensure_installed = {
-      "lua", "vim", "vimdoc", "query",
-      "bash", "json", "yaml", "toml",
-      "html", "css", "javascript", "typescript",
-      "tsx", "python", "markdown", "markdown_inline",
-      "gitcommit", "gitignore", "regex",
-    },
-    auto_install = false,
+		ensure_installed = {
+			"lua",
+			"vim",
+			"vimdoc",
+			"query",
+			"bash",
+			"json",
+			"yaml",
+			"toml",
+			"html",
+			"css",
+			"javascript",
+			"typescript",
+			"tsx",
+			"python",
+			"markdown",
+			"markdown_inline",
+			"gitcommit",
+			"gitignore",
+			"regex",
+			"rust",
+		},
+		auto_install = false,
 
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "gnn",
-        node_incremental = "grn",
-        node_decremental = "grm",
-        scope_incremental = "grc",
-      },
-    },
-  })
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "gnn",
+				node_incremental = "grn",
+				node_decremental = "grm",
+				scope_incremental = "grc",
+			},
+		},
+	})
 end
 
 return M
-
