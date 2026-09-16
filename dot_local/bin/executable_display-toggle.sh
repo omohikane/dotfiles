dd5d6192187eba75a1966878c85fa887e9724af6
r1ppl3 <39819383+omohikane@users.noreply.github.com>
2026-09-16 16:26:12 +0900

    Fix desktop output names and map tablet to DP-1

--- dot_local/bin/executable_display-toggle.sh
+++ dot_local/bin/executable_display-toggle.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 case "$1" in
     triple)
-        swaymsg output DP-3 enable
-        swaymsg output HDMI-A-1 enable
+        swaymsg output DP-2 enable
+        swaymsg output HDMI-A-2 enable
         ;;
     single)
-        swaymsg output DP-3 disable
-        swaymsg output HDMI-A-1 disable
+        swaymsg output DP-2 disable
+        swaymsg output HDMI-A-2 disable
         ;;
     dual)
-        swaymsg output DP-3 enable
-        swaymsg output HDMI-A-1 disable
+        swaymsg output DP-2 enable
+        swaymsg output HDMI-A-2 disable
         ;;
 esac
